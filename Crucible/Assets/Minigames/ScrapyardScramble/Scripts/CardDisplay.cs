4b607d5899fd28809dd56305f96a312972b398a5
Sean Hough <shough266@gmail.com>
2020-10-25 13:51:22 -0400

    finished scriptable card logic

--- Crucible/Assets/Minigames/ScrapyardScramble/Scripts/CardDisplay.cs
+++ Crucible/Assets/Minigames/ScrapyardScramble/Scripts/CardDisplay.cs
@@ -8,13 +8,17 @@ public class CardDisplay : MonoBehaviour{
 
     public Text nameText;
     public Text descriptionText;
-    public Text typetext;
+    public Text typeText;
 
     public Image artworkImage;
 
     void Start()
     {
-     
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+        typeText.text = card.type;
+        artworkImage.sprite = card.artwork;
+
     }
 
 }
