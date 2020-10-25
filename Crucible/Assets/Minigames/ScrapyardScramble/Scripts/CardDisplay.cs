d5d30a89373555ce2b19dba39f9b94f6d4af6b7b
juggler314 <71986205+juggler314@users.noreply.github.com>
2020-10-25 10:21:07 -0600

    Added cards

---
+++ Crucible/Assets/Minigames/ScrapyardScramble/Scripts/CardDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour{
+    public Card card;
+
+    public Text nameText;
+    public Text descriptionText;
+    public Text typetext;
+
+    public Image artworkImage;
+
+    void Start()
+    {
+     
+    }
+
+}
---
+++ Crucible/Assets/Minigames/ScrapyardScramble/Scripts/cardmanager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardmanager : MonoBehaviour
+{
+    public GameObject card;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(card, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
